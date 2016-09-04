43d3567707aa163d375049bd2560361b698d9e12
Fabrícia Martins <fabriciasnes@gmail.com>
2016-09-03 23:44:13 -0300

    First Commit

---
+++ Assets/AddForceScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddForceScript : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetMouseButtonDown(0) || Input.GetButtonDown("Jump"))
+		{
+			Debug.Log (KeyCode.UpArrow.ToString());
+			rb = GetComponent<Rigidbody> ();
+			rb.AddForce(0,10, 0, ForceMode.Impulse);
+		}
+
+	}
+}
